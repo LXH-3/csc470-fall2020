f81d7020a4b34df95e123dfc7fa3aaa27e5dd1f1
LXH-3 <70539599+LXH-3@users.noreply.github.com>
2020-09-10 16:02:49 -0400

    Create game01

---
+++ exercises/game01/Assets/controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed =0.1f;
+        float x = transform.position.x + transform.forward.x * speed;
+        float y = transform.position.y + transform.forward.y * speed;
+        float z = transform.position.z + transform.forward.z * speed;
+        transform.position = new Vector3(x, y, z);
+    }
+}
